1576e742aabb5baba0a8a99f0ee1ac7a14ac0f75
mrpp01 <mrpp01@gmail.com>
2016-09-20 15:46:13 -0400

    I changed the boolean method to static

--- DumbString.java
+++ DumbString.java
@@ -7,10 +7,11 @@
 public class DumbString {
 
 	/**
+	 *static
 	 * @param s String to check
 	 * @return true if all chars in s are digits ('0'-'9') and false otherwise
 	 */
-	public boolean allDigits(String s) {
+	public static boolean allDigits(String s) {
 		for (char c : s.toCharArray()) {
 			if (Character.isDigit(c) == false)
 				return false;
